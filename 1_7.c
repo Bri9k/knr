f18959c11c7ac1581dfbe213e74b41d89e25996e
Bri9k <Bri9k@protonmail.com>
2018-06-12 16:28:53 +0530

    initial

---
+++ 1_7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int c = EOF;
+	printf("%d\n",EOF);
+	//char c = EOF;
+	//printf("%c",c);
+	
+	return 0;
+}
+
+// Value of EOF turns out to be -1
+// What if I try char cinstead of int ?
+// Ans: It gives some weird ?diamond
